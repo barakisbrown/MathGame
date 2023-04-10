1428696ba489046c6dd6e381b2b0ca617b2617e3
barakisbrown <lokislayer@gmail.com>
2023-04-10 15:19:24 -0500

    This class will do the printing and asking for user input for the game itself.

---
+++ MathGame.Console/Game.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace MathGame.Console;
+
+public class Game
+{
+    public static void ShowMenu()
+    {
+        string prompt = @"Welcome to a game of math. All numbers need to be between 0 and 100.  Please select from one of the choices.
+
+        A)dd two numbers
+        S)ubtract two numbers
+        M)ultiply two numbers
+        D)ivide two numbers
+
+        Select your choice(A/S/M/D) ?
+        ";
+
+        System.Console.WriteLine(prompt);
+    }
+}
