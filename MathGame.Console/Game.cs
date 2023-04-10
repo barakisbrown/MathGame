856ebb54c9eca7f0dc9933c4dd23f15f3a18b55d
barakisbrown <lokislayer@gmail.com>
2023-04-10 16:40:11 -0500

    Added Enum and Completed ShowMenu/GetSelectedOperation Functions

--- MathGame.Console/Game.cs
+++ MathGame.Console/Game.cs
@@ -1,10 +1,14 @@
-﻿using System;
+﻿namespace MathGame.Console;
 
-namespace MathGame.Console;
+using System;
 
 public class Game
 {
-    public static void ShowMenu()
+    private int Operator1 { get; set; }
+    private int Operator2 { get; set; }
+    private GameType Type { get; set; }
+    
+    public void ShowMenu()
     {
         string prompt = @"Welcome to a game of math. All numbers need to be between 0 and 100.  Please select from one of the choices.
 
@@ -16,6 +20,49 @@ public class Game
         Select your choice(A/S/M/D) ?
         ";
 
-        System.Console.WriteLine(prompt);
+        Console.WriteLine(prompt);
+    }
+
+    public void GetSelectedOperation()
+    {
+        char key = Console.ReadKey(true).KeyChar;
+
+        switch(key)
+        {
+            case 'A':
+            case 'a':
+                Type = GameType.Addition;
+                FetchOperatorInput();
+                break;
+            case 'S':
+            case 's':
+                Type = GameType.Subtraction;
+                FetchOperatorInput();
+                break;
+            case 'M':
+            case 'm':
+                Type = GameType.Multiplication;
+                FetchOperatorInput();
+                break;
+            case 'D':
+            case 'd':
+                Type = GameType.Division;
+                FetchOperatorInput();
+                break;
+        }
+            
+    }
+
+    private void FetchOperatorInput()
+    {
+        throw new NotImplementedException();
+    }
+
+    internal enum GameType
+    {
+        Addition,
+        Subtraction,
+        Multiplication,
+        Division
     }
 }
