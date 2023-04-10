c0117ad714cf46c9628dc25eebcd34a98a2af2e9
barakisbrown <lokislayer@gmail.com>
2023-04-10 16:39:38 -0500

    Validate the Input when converting strings to int

---
+++ MathGame.Console/Helpers.cs
@@ -0,0 +1,17 @@
+﻿namespace MathGame.Console;
+
+using System;
+
+internal class Helpers
+{
+    internal static string ValidateResult(string ?result)
+    {
+        while(string.IsNullOrEmpty(result) || !Int32.TryParse(result, out _))
+        {
+            Console.WriteLine("Your answer needs to be an integer. Please try again!");
+            Console.Write("X = ");
+            result = Console.ReadLine();
+        }
+        return result;
+    }
+}
