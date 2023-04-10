5a832c31b5d06e193ef3919883a1d30d9754ec1d
barakisbrown <lokislayer@gmail.com>
2023-04-10 15:01:48 -0500

    Add project files.

---
+++ MathGame.Console/Calc.cs
@@ -0,0 +1,19 @@
+﻿namespace MathGame.Console;
+
+public class Calc
+{
+    public static int Add(int Operator1, int Operator2) => Operator1 + Operator2;
+    
+    
+    public static int Substract(int Operator1, int Operator2) => Operator1 - Operator2;
+
+    public static int Multiply(int Operator1, int Operator2) => Operator1 * Operator2;
+
+    public static int? Division(int Dividend, int Divisor) 
+    {
+        if (Divisor == 0)
+            return null;
+        else
+            return Dividend / Divisor;
+    }
+}
---
+++ MathGame.Console/Program.cs
@@ -0,0 +1,13 @@
+﻿
+
+using MathGame.Console;
+
+var result = Calc.Division(10, 3);
+
+if (result is null)
+{
+    Console.WriteLine("Error. Division by zero is illegal.");
+} else
+{
+    Console.WriteLine($"{result}");
+}
