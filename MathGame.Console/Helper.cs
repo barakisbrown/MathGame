a8983cc46e6f97569450e4d876b0581b31827ae6
barakisbrown <lokislayer@gmail.com>
2023-04-10 22:20:21 -0500

    Added a helper class to help validate input coverting string to int

---
+++ MathGame.Console/Helper.cs
@@ -0,0 +1,19 @@
+﻿
+namespace MathGame.Console;
+
+using System;
+
+
+internal class Helper
+{
+    internal static string ValidateResults(string? result)
+    {
+        while (string.IsNullOrEmpty(result) || !Int32.TryParse(result, out _))
+        {
+            Console.WriteLine("Your answer is an integer. Try again!");
+            Console.Write("X = ");
+            result = Console.ReadLine();
+        }
+        return result;
+    }
+}
