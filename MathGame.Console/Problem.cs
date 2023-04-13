ef8b694a3c22785dfa3cb93181d8fb2d95e8204c
barakisbrown <lokislayer@gmail.com>
2023-04-13 16:56:58 -0500

    Added a Problem Class so that I can store it in a list to show the user.

---
+++ MathGame.Console/Problem.cs
@@ -0,0 +1,12 @@
+﻿
+namespace MathGame.Console;
+
+
+
+internal class Problem
+{
+    public string? Equation { get; set; }
+    public DateTime Date { get; set; }
+    public int UserAnswer { get; set; }
+    public int ActualAnswer { get; set; }
+}
