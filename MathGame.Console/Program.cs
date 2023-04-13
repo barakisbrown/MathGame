975ca0954a08e2a7e8989b686ef0004830c89650
barakisbrown <lokislayer@gmail.com>
2023-04-12 21:01:02 -0500

    Changed it where all the game logic is inside the game class only

--- MathGame.Console/Program.cs
+++ MathGame.Console/Program.cs
@@ -2,12 +2,5 @@
 
 using MathGame.Console;
 
-var result = Calc.Division(10, 3);
-
-if (result is null)
-{
-    Console.WriteLine("Error. Division by zero is illegal.");
-} else
-{
-    Console.WriteLine($"{result}");
-}
+Game game = new();
+game.BeginGame();
